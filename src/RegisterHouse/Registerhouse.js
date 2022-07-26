4376284a012a61ed8b50f71495210b6e372562fa
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-26 11:58:58 +0300

    Added css to my regiterHouse component

--- src/RegisterHouse/Registerhouse.js
+++ src/RegisterHouse/Registerhouse.js
@@ -3,44 +3,73 @@ import "./RegisterHouse.css";
 function Registerhouse()
 {
     return (
-        <div className="registerhouse">
-            <h2>Please fill in this form when you want to register a House</h2>
-                <form className="registerHouseA">
-                <label>Name :</label>
-                <input placeholder="enter name" name="name"/>
-                <label>Age :</label>
-                <input placeholder="age" name="age"/>
-                <label>Type of House:</label>
-                <select></select>
-                <label>Number of Rooms</label>
-                <select>
-                    <option value="Apartment">Apartment</option>
-                    <option value="Bungalow">Bungalow</option>
-                    <option value="Mansion">Mansion</option>
-                </select>
-                <label>House Location</label>
-                <select>
-                    <option value="Nairobi">Nairobi</option>
-                    <option value="Mombasa">Mombasa</option>
-                    <option value="Nakuru">Nakuru</option>
-                    <option value="Eldoret">Eldoret</option>
-                </select>
-                <label>Number of Bedrooms</label>
-                <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6 and more">6 and more</option>
-                </select>
-                <label>Preferred view day</label>
-                <select></select>
-                <input type="submit"/>
-                </form>
+        <div>
+            <section className="registerHouse" >
+                <div className="registerHouseA">
+                    <div className="register-box">
+                        <div className="left"></div>
+                        <div className="right">
+                        <form id="formYetu"> 
+                         <h2>Register with Us</h2>
+                         <input type="text" className="field" placeholder="Your Name" />
+                         <input type="text" className="field" placeholder="Your Age" />
+                         <input type="text" className="field" placeholder="Phone Number" />
+                         <label>Type of House
+                         <select>
+                            <option value="Apartment">Apartment</option>
+                            <option value="Bungalow">Bungalow</option>
+                            <option value="Mansion">Mansion</option>
+                         </select>
+                         </label>
+                         <br />
+                         <label>House Location
+                        <select>
+                            <option value="Nairobi">Nairobi</option>
+                            <option value="Mombasa">Mombasa</option>
+                            <option value="Nakuru">Nakuru</option>
+                            <option value="Eldoret">Eldoret</option>
+                        </select>
+                        </label>
+                        <br />
+                        <label>Number of Bedrooms
+                        <select>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6 and more">6 and more</option>
+                        </select>
+                        </label>
+                        <br />
+                        <label>Number of Bedrooms
+                        <select>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6 and more">6 and more</option>
+                        </select>
+                        </label>
+                        <label>Preffered Viewday
+                        <select>
+                            <option value="sunday">Sunday</option>
+                            <option value="monday">Monday</option>
+                            <option value="tuesday">Tuesday</option>
+                            <option value="wednesday">Wednesday</option>
+                            <option value="thursday">Thursday</option>
+                            <option value="friday">Friday</option>
+                            <option value="saturday">Saturday</option>
+                        </select>
+                        </label>
+                         <button className="registerbtn">Send</button>
+                     </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
-    
-        
 }
 export default Registerhouse;
